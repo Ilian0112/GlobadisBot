7afea8935d59ea0e63fcdcfbfaeae86c14f26059
Ilian0112 <38587349+Ilian0112@users.noreply.github.com>
2018-12-02 00:30:09 +0100

    Update globadisbot.js

--- .gitignore/globadisbot.js
+++ .gitignore/globadisbot.js
@@ -1,6 +1,5 @@
 // MODULE
 const Discord = require(`discord.js`);
-const firebase = require("firebase");
 //
 
 // NEW CLIENT ( les deux pour pas ce prendre la tête )
@@ -10,47 +9,15 @@ const bot = new Discord.Client();
 
 // BOT INFO
 const version = `V.0.0.1`
-const PREFIX = `!`
+const PREFIX = `globadis.`
 const botname = `GlobadisBot`
 //
 
 // Quand le BOT est start
 bot.on(`ready`, function () {
     console.log(`${botname} connectée !`);
-    bot.users.setAvatar("https://cdn.discordapp.com/attachments/517353858847080451/517802249553051688/Sans_titre1.jpg")
+    bot.user.setActivity(`GlobadisMCPE`)
 })
-//
-
-// Date Base
-var admin = require("firebase-admin");
-console.log("Connecting to Firebase...");
-admin.initializeApp({credential: admin.credential.cert(JSON.parse(process.env.GOOGLE_ACC)),
-                    databaseURL: "https://globadisbot-db.firebaseio.com"});
-console.log("Connected successfully!");
-
-const db = admin.firestore();
-const settings = {timestampsInSnapshots: true};
-
-var BD = db.collection("Users")
-//
-
-// Système XP
-function SaveXp(UserId, Xp){
-  //Tu met ton code de traitement de level, etc...
-
-  var o = BD.doc(UserId); // on recupere le document de l'utilisateur pas sont identifiant discord
-  o.get().then(function(docs){ // on recuper les données du document
-    var Data
-    if(!docs.exists){ // si les donnée utilisateur n'existe pas
-      //Ici je met juste l'xp de l'utilisateur, mais tu modifira pour d'autres donnée
-      Data = {"Xp": Xp}
-    }else{
-      var DataTemp = docs.data().Xp //La table a l'interieur du document
-      var Data = {"Xp": DataTemp.Xp + Xp}
-    }
-    o.set(Data) // on enregistre
-  })
-}
 //
 
 // Le code 
@@ -58,11 +25,9 @@ bot.on("message", async function(message) {
     if (message.author.equals(bot.user)) return;
 
     if (!message.content.startsWith(PREFIX)) return;
-      
-    SaveXp(message.author.id, 20) // donne 20 xp a l'utilisateur qui a envoyer le message
-      
-    var args = message.content.substring(PREFIX.length).split (` `);
 
+    var args = message.content.substring(PREFIX.length).split (` `);
+  
     var args2 = message.content.split(` `).slice(1);
 
     var suffix = args2.join(` `);
@@ -81,14 +46,76 @@ bot.on("message", async function(message) {
 
     var foother = `Commande effectué par @` + message.author.username + `#` + message.author.discriminator + ` ! | GlobadisBot - ` + version
     
-    var modlog = member.guild.channels.find(`name`, `log`)
+    var modlog = member.guild.channels.find(`name`, `logs`)
     
+// Level Système
+
+//
+
+
     var user = message.mentions.users.first();
+
+        var noperm_embed = new Discord.RichEmbed()
+            .setTitle(`⚠Erreur⚠`)
+            .setDescription(`Vous n'êtes pas autoriser à effectuer cette commande !`)
+            .setFooter(foother)
+            .setColor(`#FF0000`)
+            .setTimestamp()
+
+        var nologschannel_embed = new Discord.RichEmbed()
+            .setTitle(`⚠Erreur⚠`)
+            .setDescription(`Le channel __**#logs**__ est introuvable merci de le créé.`)
+            .setFooter(foother)
+            .setColor(`#FF0000`)
+            .setTimestamp()
+        
+        var noreason_embed = new Discord.RichEmbed()
+            .setTitle(`⚠Erreur⚠`)
+            .setDescription(`Vous avez oublié la raison de la sanction.`)
+            .setFooter(foother)
+            .setColor(`#FF0000`)
+            .setTimestamp()
 
     switch (args[0].toLowerCase()) {
         case `mute`:
             message.delete()
 
+            var norolemute_embed = new Discord.RichEmbed()
+                .setTitle(`⚠Erreur⚠`)
+                .setDescription(`Le rôle __**Mute**__ est introuvable !`)
+                .setFooter(foother)
+                .setColor(`#FF0000`)
+                .setTimestamp()
+
+            var nomention_embed = new Discord.RichEmbed()
+                .setTitle(`⚠Erreur⚠`)
+                .setDescription(`Vous devez mentionner une personne afin de la réduire au silence !`)
+                .setFooter(foother)
+                .setColor(`#FF0000`)
+                .setTimestamp()
+
+            if(!message.member.hasPermission(`MUTE_MEMBERS`)) return message.channel.send(noperm_embed);
+            if(!modlog) return message.channel.send(nologschannel_embed);  
+            if(!roleMute) return message.channel.send(norolemute_embed)
+            var member = message.mentions.members.first();
+            if (message.mentions.users.size < 1) return message.channel.send(nomention_embed)
+            if(!reason) return message.channel.send(noreason_embed)
+        
+            message.channel.send(`${member.toString()} a bien été réduit au silence !`)
+            member.addRole(roleMute)
+        
+                var mute_embed = new Discord.RichEmbed()
+                    .setAuthor(`Sanction : Mute`)
+                        .addField(`Modérateur :`, `${message.author.username}#${message.author.discriminator}`)
+                        .addField(`Utilisateur :`, `${user.toString()}`, true)
+                        .addField(`ID Utilisateur :`, `${user.id}`)
+                        .addField(`Raison :`, `${reason}`)
+                    .setFooter(foother)
+                    .setTimestamp()
+                    .setColor(`#FF0000`)
+            
+            modlog.send(mute_embed)
+            
         break;
        
         case `tempmute`:
@@ -99,47 +126,161 @@ bot.on("message", async function(message) {
         case `unmute`:
             message.delete()
 
+            var nomention_embed = new Discord.RichEmbed()
+                .setTitle(`⚠Erreur⚠`)
+                .setDescription(`Vous devez mentionner une personne afin de révoquer la sanction !`)
+                .setFooter(foother)
+                .setColor(`#FF0000`)
+                .setTimestamp()
+
+            if(!message.member.hasPermission(`MUTE_MEMBERS`)) return message.channel.send(noperm_embed);
+            var member = message.mentions.members.first();
+            if(message.mentions.users.size < 1) return message.channel.send(nomention_embed)
+            //if(message.member.roles.find(`name`, `Mute`)) {
+                    var unmute_embed = new Discord.RichEmbed()
+                        .setAuthor(`Sanction révoquer : Mute`)
+                        .addField(`Modérateur :`, `${message.author.username}#${message.author.discriminator}`)
+                        .addField(`Utilisateur :`, `${user.toString()}`, true)
+                        .addField(`ID Utilisateur :`, `${user.id}`)
+                    .setFooter(foother)
+                    .setTimestamp()
+                    .setColor(`#34C924`)
+                member.removeRole(roleMute)
+                modlog.send(unmute_embed)
+                message.channel.send(`${member.toString()} peut désormais parler !`)
+            /*} else {    
+                    var nonmute_embed = new Discord.RichEmbed()
+                        .setTitle(`⚠Erreur⚠`)
+                        .setDescription(`${user.toString()} n'est pas réduit au silence !`)
+                        .setFooter(foother)
+                        .setColor(`#FF0000`)
+                        .setTimestamp()
+                
+                message.channel.send(nonmute_embed)
+            } */
         break;
         
         case `kick`:
             message.delete()
 
+                var nomention_embed = new Discord.RichEmbed()
+                    .setTitle(`⚠Erreur⚠`)
+                    .setDescription(`Vous devez mentionner une personne afin de l'expulser !`)
+                    .setFooter(foother)
+                    .setColor(`#FF0000`)
+                    .setTimestamp()
+
+            if(!message.member.hasPermission(`KICK_MEMBERS`)) return message.channel.send(noperm_embed);
+            var member = message.mentions.members.first();
+            if(message.mentions.users.size < 1) return message.channel.send(nomention_embed)
+            if(!reason) return message.channel.send(noreason_embed)
+
+            message.channel.send(`${member.toString()} a bien été expusler du serveur !`)
+
+                var kick_embed = new Discord.RichEmbed()
+                    .setAuthor(`Sanction : Kick`)
+                        .addField(`Modérateur :`, `${message.author.username}#${message.author.discriminator}`)
+                        .addField(`Utilisateur :`, `${user.toString()}`, true)
+                        .addField(`ID Utilisateur :`, `${user.id}`)
+                        .addField(`Raison :`, `${reason}`)
+                    .setFooter(foother)
+                    .setTimestamp()
+                    .setColor(`#FF0000`)
+
+            modlog.send(kick_embed)
+
+           message.guild.member(user).send(kick_embed)
+
+           member.kick(reason)
         break;
 
         case `ban`:
             message.delete()
 
+                var nomention_embed = new Discord.RichEmbed()
+                    .setTitle(`⚠Erreur⚠`)
+                    .setDescription(`Vous devez mentionner une personne afin de le bannir !`)
+                    .setFooter(foother)
+                    .setColor(`#FF0000`)
+                    .setTimestamp()
+
+            if(!message.member.hasPermission(`BAN_MEMBERS`)) return message.channel.send(noperm_embed);
+            var member = message.mentions.members.first();
+            if(message.mentions.users.size < 1) return message.channel.send(nomention_embed)
+            if(!reason) return message.channel.send(noreason_embed)
+
+            message.channel.send(`${member.toString()} a bien été banni du serveur !`)
+
+                var ban_embed = new Discord.RichEmbed()
+                    .setAuthor(`Sanction : Bannissement`)
+                        .addField(`Modérateur :`, `${message.author.username}#${message.author.discriminator}`)
+                        .addField(`Utilisateur :`, `${user.toString()}`, true)
+                        .addField(`ID Utilisateur :`, `${user.id}`)
+                        .addField(`Raison :`, `${reason}`)
+                    .setFooter(foother)
+                    .setTimestamp()
+                    .setColor(`#FF0000`)
+
+            modlog.send(ban_embed)
+
+            message.guild.member(user).send(ban_embed)
+
+            member.ban(reason)
         break;
        
         case `unban`:
             message.delete()
+                
+                var noid_embed = new Discord.RichEmbed()
+                    .setTitle(`⚠Erreur⚠`)
+                    .setDescription(`Vous devez écrire l'ID de la personne à débannir !`)
+                    .setFooter(foother)
+                    .setColor(`#FF0000`)
+                    .setTimestamp()
 
+            if(!message.member.hasPermission(`BAN_MEMBERS`)) return message.channel.send(noperm_embed);
+            let IDBanned = message.content.split(" ").slice(1);
+            let suffix_IDBanned = IDBanned.join('%20')
+            if(!suffix_IDBanned) return message.channel.send(noid_embed)
+
+                var unban_embed = new Discord.RichEmbed()
+                    .setAuthor(`Sanction révoquer : Ban`)
+                        .addField(`Modérateur :`, `${message.author.username}#${message.author.discriminator}`)
+                        .addField(`Utilisateur :`, `${suffix_IDBanned}`)
+                    .setFooter(foother)
+                    .setTimestamp()
+                    .setColor(`#34C924`)
+                
+            modlog.send(unban_embed)
+            guild.unban(suffix_IDBanned)
+            message.channel.send(`**${suffix_IDBanned}** à bien été unban !`)
         break;       
 
         case `level`:
             message.delete()
-
+            
         break;       
 
         case `help`:
-        if(message.author.bot) return message.channel.send(`Erreur, les bot ne peuve utiliser mes commandes.`)
-            message.delete()
+            if(message.author.bot) return message.channel.send(`Erreur, les bot ne peuve utiliser mes commandes.`)
+                message.delete()
 
-                var help_embed = new Discord.RichEmbed()
-                    .setTitle(`Menu d'aide`)
-                        .addField(PREFIX + `mute`, `Cette commande vous permet de réduire au silence une personne.`)
-                        .addField(PREFIX + `tempmute`, `Cette commande vous permet de réduire au silence une personne durant la durée de votre choix.`)
-                        .addField(PREFIX + `unmute`, `Cette commande vous permet de rendre la possibilité de parler à une personne réduite au silence.`)
-                        .addField(PREFIX + `kick`, `Cette commande vous permet d'expulser une personne du serveur.`)
-                        .addField(PREFIX + `ban`, `Cette commande vous permet de bannir une personne du serveur.`)
-                        .addField(PREFIX + `unban`, `Cette commande vous permet de révoquer le bannissement d'une personne du serveur.`)
-                        .addBlankField()
-                        .addField(PREFIX + `level`, `Cette commande vous permet de voir votre niveau ou celui d'un autre membre.`)
-                        .addField(PREFIX + `help`, `Cette commande vous permet d'affichier l'aide.`)
-                    .setColor(`#53A338`)
-                    .setFooter(foother)
-            message.channel.send(help_embed)
-        break;           }
+                    var help_embed = new Discord.RichEmbed()
+                        .setTitle(`Menu d'aide`)
+                            .addField(PREFIX + `mute`, `Cette commande vous permet de réduire au silence une personne.`)
+                            .addField(PREFIX + `tempmute`, `**[OFF]** | Cette commande vous permet de réduire au silence une personne durant la durée de votre choix.`)
+                            .addField(PREFIX + `unmute`, `Cette commande vous permet de rendre la possibilité de parler à une personne réduite au silence.`)
+                            .addField(PREFIX + `kick`, `Cette commande vous permet d'expulser une personne du serveur.`)
+                            .addField(PREFIX + `ban`, `Cette commande vous permet de bannir une personne du serveur.`)
+                            .addField(PREFIX + `unban`, `Cette commande vous permet de révoquer le bannissement d'une personne du serveur.`)
+                            .addBlankField()
+                            .addField(PREFIX + `level`, `**[OFF]** | Cette commande vous permet de voir votre niveau ou celui d'un autre membre.`)
+                            .addField(PREFIX + `help`, `Cette commande vous permet d'affichier l'aide.`)
+                        .setColor(`#53A338`)
+                        .setFooter(foother)
+                message.channel.send(help_embed)
+        break;           
+    }
 });
 
 bot.login(process.env.TOKEN);
